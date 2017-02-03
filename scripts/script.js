6bc02f19f47ad7d7e91ead9b48a14122f445f91c
Fauziku2 <M.fauzi_87@hotmail.com>
2017-02-03 20:47:10 +0800

    added mousemove feature

--- scripts/script.js
+++ scripts/script.js
@@ -155,4 +155,13 @@ function draw () {
   y += dy
 }
 
+document.addEventListener('mousemove', mouseMoveHandler)
+
+function mouseMoveHandler (e) {
+  var relativeX = e.clientX - canvas.offsetLeft
+  if (relativeX > paddleWidth / 2 && relativeX < canvas.width - (paddleWidth / 2)) {
+    paddleX = relativeX - paddleWidth / 2
+  }
+}
+
 setInterval(draw, 10)
